fb0128510c3c73f5a9aa5bc9ec16e79b2279d2e0
Miguel-Ubuntu-lap <mgarlcr@gmail.com>
2022-06-01 20:56:42 -0400

    Added BPs and Views

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -6,9 +6,7 @@ def create_app():
     @app.route('/')
     def hello():
         return '<style> h1 { text-align: center} h1:hover { color: skyblue} div {height:100vh} </style> <h1>Hello, this is PetFax</h1>'     
-
-        # pets index route
-    @app.route('/pets')
-    def pets():
-        return 'These are our pets available for adoption'
+    from . import pet
+    app.register_blueprint(pet.bp)   
+     
     return app
